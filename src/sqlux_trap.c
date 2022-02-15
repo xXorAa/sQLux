937ecca865676a380c481bcea0e86000daba58ba
Graeme Gregory <graeme@xora.org.uk>
2022-02-15 18:58:16 +0000

    src/sqlux_trap.c : need to move context stores outside this function
    Signed-off-by: Graeme Gregory <graeme@xora.org.uk>

--- src/sqlux_trap.c
+++ src/sqlux_trap.c
@@ -11,13 +11,7 @@ void sqlux_trap(int trapno, int d0)
 	int i;
 
 	DEBUG_PRINT("Starting TRAP %d\n", trapno);
-	sqlux_store_main_ctx();
-	//sqlux_restore_trap_ctx();
 
-	for (i = 0; i < 15; i++) {
-		m68k_set_reg(M68K_REG_D0 + i,
-			m68k_get_reg(main_ctx, M68K_REG_D0 + i));
-	}
 	m68k_set_reg(M68K_REG_PC, 0);
 
 	sqlux_in_trap = 1;
@@ -26,8 +20,7 @@ void sqlux_trap(int trapno, int d0)
 	m68k_execute(100000);
 
 	sqlux_in_trap = 0;
-	sqlux_store_trap_ctx();
-	sqlux_restore_main_ctx();
+
 	DEBUG_PRINT("Stoping TRAP %d\n", trapno);
 }
 
